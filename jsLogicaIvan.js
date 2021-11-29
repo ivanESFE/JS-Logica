0a322cc20f43dea2092e1ae63b34a71009a5dc3f
ivan02 <ivan.esteban@institutvidreres.cat>
2021-11-29 09:29:45 +0100

    Ex2-logica

--- jsLogicaIvan.js
+++ jsLogicaIvan.js
@@ -1,11 +1,8 @@
-let numero = prompt('Introdueix el numero')
+let num1 = 20
+let num2 = 9
 
-if (numero % 3 ==0) {
-    console.log('El numero ' + numero + ' és divisible de 3' )
-}
-else if(numero % 7==0){
-    console.log('El numero ' + numero + ' és divisible de 7' )
-}
-else{
-    console.log('El numero ' + numero + ' no es divisible de 3 i 7' )
-}
+num1 >= 10 && num1<=50 ? console.log(num1 + ' està entre 10 i 50')
+                       : console.log(num1 + ' no està entre 10 i 50')
+
+num2 >= 10 && num1<=50 ? console.log(num2 + ' està entre 10 i 50')
+                       : console.log(num2 + ' no està entre 10 i 50')
