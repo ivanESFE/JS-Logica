f33cc264acb7a55406a132981f4ac51f8ac95c67
ivan02 <ivan.esteban@institutvidreres.cat>
2021-12-01 23:06:37 +0100

    Ex5-Logica

--- jsLogicaIvan.js
+++ jsLogicaIvan.js
@@ -1,5 +1,8 @@
-var x ="*"
-for (let i = 0; i <=5; i++) {
-    console.log(x.repeat(i))
-    
+var nom = "hol3 c1m2 t5al"
+var num = nom.replace(/[^0-9]/gi, "")
+let sum =0
+
+for (let i = 0; i < num.length; i++) {
+    sum += Number(num.charAt(i))
 }
+console.log(sum)
